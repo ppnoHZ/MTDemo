23927a39b7c01a9541a060a999ed957bdb23ee55
zhoudd-mac <zhoudd@stark.tm>
2015-08-27 17:47:08 +0800

    更新

--- notification/notification.js
+++ notification/notification.js
@@ -1,34 +1,39 @@
 if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault('counter', 0);
+  // counter starts at 0
+  Session.setDefault('counter', 0);
 
-    Template.hello.helpers({
-        counter: function () {
-            return Session.get('counter');
-        }
-    });
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
 
-    Template.hello.events({
-        'click button': function () {
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
 
-            // increment the counter when button is clicked
-            if (Meteor.isCordova)
-            {
-                alert("notification");
-                Message.show();
-            }
-            if(Meteor.isClient)
-            {
-                Message.log("通知");
-            }
-
-            //Session.set('counter', Session.get('counter') + 1);
-        }
-    });
+      if (Meteor.isCordova)
+      {
+        //alert("notification");
+        var data={
+          id:Session.get('counter'),
+          title:"系统通知",
+          text:"已发货！"
+        };
+        Message.show(data);
+      }
+      if(Meteor.isClient)
+      {
+        Message.log("通知");
+        //alert("notification");
+      }
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
-    });
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
 }
--- notification/packages/notification/notification-cordova.js
+++ notification/packages/notification/notification-cordova.js
@@ -2,25 +2,27 @@
  * Created by ID on 15/8/21.
  */
 
-Message.show = function () {
+Message.show = function (data) {
     cordova.plugins.notification.local.hasPermission(function (granted) {
         console.log('Permission has been granted: ' + granted);
-    });
-
-    cordova.plugins.notification.local.registerPermission(function (granted) {
-        console.log('Permission has been granted: ' + granted);
+        if(!granted)
+        {
+            cordova.plugins.notification.local.registerPermission(function (granted) {
+                console.log('Permission has been granted: ' + granted);
+            });
+        }
     });
 
     //console.log(cordova.plugins.notification.local);
     cordova.plugins.notification.local.schedule({
-        id: 1,
-        title: "Test,Message",
-        text: "测试消息",
-        data: {id: "data.id"}
+        id: data.id,
+        title: data.title,
+        text: data.text,
+        data: {key: "data.id"}
     });
     cordova.plugins.notification.local.on('click', function (notification) {
         alert(notification.id);
-        alert(notification.data.id);
+        alert(notification.data.key);
         //showToast('id:' + notification.id);
 
     });
