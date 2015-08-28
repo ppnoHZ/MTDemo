907447e5732dc3c4b30b4856e052ab5bcd17b027
zhoudd-mac <zhoudd@stark.tm>
2015-08-28 14:12:31 +0800

    添加 批处理文件

---
+++ simple-map/mobile-config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ID on 15/8/28.
+ * Author:zhoudd
+ * email:zhoudd@stark.tm
+ */
+// Used in order to not get browser cross browser origin errors
+// Typically you want to get more specific than this
+App.accessRule("*");
