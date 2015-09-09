eb4b0fa5b5322240782430e1ce6ab66d320d6645
zhoudd-mac <zhoudd@stark.tm>
2015-09-09 15:54:00 +0800

    提交自动更新实验结果

---
+++ autoupdateDemo1/mobile-config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by ID on 15/8/28.
+ * Author:zhoudd
+ * email:zhoudd@stark.tm
+ */
+// Used in order to not get browser cross browser origin errors
+// Typically you want to get more specific than this
+App.accessRule("*");
+App.info({
+    //id: 'com.stark.rsc.demo',
+    name: '日升昌',
+    description: '大宗商品交易',
+    author: '斯塔克',
+    email: 'contact@example.com',
+    website: '192.168.3.104:3000'
+});
