bd39e8253aaf82b6c5fe42f50226693b62b2658b
zhoudd-mac <zhoudd@stark.tm>
2015-09-15 15:09:52 +0800

    添加二维码识别Demo

---
+++ barcodeScannerDemo/barcodeScannerDemo.js
@@ -0,0 +1,37 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+
+      cordova.plugins.barcodeScanner.scan(
+          function (result) {
+            alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+          },
+          function (error) {
+            alert("Scanning failed: " + error);
+          }
+      );
+
+
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
