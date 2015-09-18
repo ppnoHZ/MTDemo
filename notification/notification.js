87aeac515f95cad33fddcca2e134619f0234462a
zhoudd-mac <zhoudd@stark.tm>
2015-09-18 17:53:11 +0800

    update

--- notification/notification.js
+++ notification/notification.js
@@ -1,7 +1,25 @@
 if (Meteor.isClient) {
     // counter starts at 0
     Session.setDefault('counter', 0);
-
+    cordova.plugins.notification.local.hasPermission(function (granted) {
+        //console.log('[hasPermission] Permission has been granted: ' + granted);
+        /*if(!granted)
+         {
+         cordova.plugins.notification.local.registerPermission(function (granted) {
+         console.log('Permission has been granted: ' + granted);
+         });
+         }*/
+    });
+    cordova.plugins.notification.local.registerPermission(function (granted) {
+        console.log('[registerPermission] Permission has been granted: ' + granted);
+    });
+    cordova.plugins.notification.local.on('click', function (notification) {
+        alert(notification.id);
+        alert(JSON.stringify(notification));
+        //alert(JSON.stringify(notification.data));
+        alert(JSON.parse(notification.data));
+        //showToast('id:' + notification.id);
+    });
     Template.hello.helpers({
         counter: function () {
             return Session.get('counter');
@@ -19,23 +37,12 @@ if (Meteor.isClient) {
                  title:"系统通知",
                  text:"已发货！"
                  };*/
-
-                cordova.plugins.notification.local.hasPermission(function (granted) {
-                    //console.log('[hasPermission] Permission has been granted: ' + granted);
-                    /*if(!granted)
-                     {
-                     cordova.plugins.notification.local.registerPermission(function (granted) {
-                     console.log('Permission has been granted: ' + granted);
-                     });
-                     }*/
-                });
-                cordova.plugins.notification.local.registerPermission(function (granted) {
-                    console.log('[registerPermission] Permission has been granted: ' + granted);
-                });
                 console.log('[show message]');
                 cordova.plugins.notification.local.schedule({
-                    title: "Meeting in 15 minutes!",
-                    text: "Jour fixe Produktionsbesprechung",
+                    id: Session.get('counter'),
+                    title: "系统通知",
+                    text: "已发货！",
+                    data: {key: "20150101010010"}
                 });
                 //Message.show();
             }
@@ -44,7 +51,7 @@ if (Meteor.isClient) {
              Message.log("通知");
              //alert("notification");
              }*/
-            //Session.set('counter', Session.get('counter') + 1);
+            Session.set('counter', Session.get('counter') + 1);
             //Session.set('counter', Session.get('counter') + 1);
         }
     });
